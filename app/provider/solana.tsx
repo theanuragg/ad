37930b9d9b5ca012075486771867bbfa448bc7ff
anurag <anurags9693@gmail.com>
2025-11-17 17:12:57 +0530

    add

--- app/provider/solana.tsx
+++ app/provider/solana.tsx
@@ -16,7 +16,7 @@ interface SolanaProviderProps {
 
 export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.mainnet-beta;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
